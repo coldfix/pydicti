be9676d2caa349c38f4944378722bc27fae71d29
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-29 16:40:11 +0100

    New minor version
    CHANGES:
    
     - fix dicti.pop
     - support deepcopy(dicti)
     - make nosetest automatically execute the doctests

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 setup(
     name='pydicti',
-    version='0.0.1',
+    version='0.0.2',
     description='Case insensitive derivable dictionary',
     long_description=open('README.rst').read(),
     author='Thomas Gläßle',

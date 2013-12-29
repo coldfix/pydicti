08eaeba19f08f472d905656be27f10fbcf215c5a
Thomas Gläßle <t_glaessle@gmx.de>
2013-12-29 16:36:30 +0100

    Convert README to reStructuredText
    reST READMEs interact better with PyPI.

--- setup.py
+++ setup.py
@@ -3,17 +3,11 @@
 
 from distutils.core import setup
 
-try:
-    long_description = open('README.rst').read()
-except:
-    long_description = None
-
-# invoke distutils
 setup(
     name='pydicti',
     version='0.0.1',
     description='Case insensitive derivable dictionary',
-    long_description=long_description,
+    long_description=open('README.rst').read(),
     author='Thomas Gläßle',
     author_email='t_glaessle@gmx.de',
     url='https://github.com/coldfix/pydicti',
